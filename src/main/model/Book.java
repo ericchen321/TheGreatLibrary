205c26c9bb23711284eda68774ef8050bbc11b3e
eric-chen <i0y0b@ugrad.cs.ubc.ca>
2018-09-13 22:32:17 -0700

    allows multiple books added to the book collection

--- src/main/model/Book.java
+++ src/main/model/Book.java
@@ -2,13 +2,14 @@ package model;
 
 public class Book {
     public enum Genre {
-        ART, BIOGRAPHY, CLASSICS, FANTASY, HISTORY, NONFICTION
+        ART, BIOGRAPHY, CLASSICS, FANTASY, HISTORY, FICTION, UNCATAGORIZED
     }
     private String name;
     private String author;
     private int yearOfPublish;
-    Genre genre;
+    private Genre genre;
 
+    // setters and getters
     public void setBookName(String name) {
         this.name = name;
     }
@@ -18,7 +19,51 @@ public class Book {
     }
 
     public void setYearOfPublish(int yOfp) {
-        this.yearOfPublish = yOfp;
+        yearOfPublish = yOfp;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getBookName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublish() {
+        return yearOfPublish;
+    }
+
+    public Genre getGenre(){
+        return genre;
+    }
+
+    // convert string genre to enum typed genre
+    public Genre resolveGenre(String genre){
+        switch (genre) {
+            case "art":
+                return Genre.ART;
+            case "biography":
+                return Genre.BIOGRAPHY;
+            case "classics":
+                return Genre.CLASSICS;
+            case "fantasy":
+                return Genre.FANTASY;
+            case "history":
+                return Genre.HISTORY;
+            case "fiction":
+                return Genre.FICTION;
+            default:
+                return Genre.UNCATAGORIZED;
+        }
+    }
+
+    public String toString(){
+        return name + " by " + author + " published in " + yearOfPublish;
     }
 
 
--- src/main/ui/TheGreatLibrary.java
+++ src/main/ui/TheGreatLibrary.java
@@ -1,11 +1,18 @@
 package ui;
 
+import model.Book;
+import java.util.*;
+
 public class TheGreatLibrary {
+    List<Book> listOfBooks = new ArrayList<Book>();
+    Scanner scanner = new Scanner(System.in);
 
     // constructor
     public TheGreatLibrary(){
         printWelcomeMsg();
         printIntro();
+        addBooks();
+        printAllBooks();
     }
 
     // print the welcome message
@@ -16,6 +23,45 @@ public class TheGreatLibrary {
     // print the intro
     public void printIntro(){
         System.out.println("This is a personal library manager.");
+    }
+
+    // add books to the library
+    public void addBooks(){
+        Book book = new Book();
+
+        while (true) {
+            System.out.println("Please enter book name:");
+            book.setBookName(scanner.nextLine());
+            System.out.println("Please enter author's name:");
+            book.setBookAuthor(scanner.nextLine());
+            System.out.println("Please enter year of publish:");
+            book.setYearOfPublish(scanner.nextInt());
+            scanner.nextLine();
+            System.out.println("Please enter the genre:");
+            book.setGenre(book.resolveGenre(scanner.nextLine()));
+            System.out.println("Now let's confirm the book's info:");
+            System.out.println("book's name: " + book.getBookName());
+            System.out.println("book's author: " + book.getAuthor());
+            System.out.println("book's publish year:" + book.getYearOfPublish());
+            System.out.println("book genre:" + book.getGenre());
+            System.out.println("Confirm add to your library? y/n");
+            if (scanner.nextLine().equals("y")) {
+                listOfBooks.add(book);
+                System.out.println("Book added!");
+            }
+            System.out.println("Want to add another book? y/n");
+            if (scanner.nextLine().equals("y")) {
+                book = new Book();
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    // print all books in the library
+    public void printAllBooks(){
+        System.out.println(listOfBooks);
     }
 
     // star TheGreatLibrary app
