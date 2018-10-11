a4a5f916c4c198f0e316c4b1ac314df31f391812
eric-chen <i0y0b@ugrad.cs.ubc.ca>
2018-10-10 23:13:46 -0700

    added import book edition GUI; hooked it up with backend

--- src/main/ui/operations/ImportBookEdOperation.java
+++ src/main/ui/operations/ImportBookEdOperation.java
@@ -1,11 +1,15 @@
 package ui.operations;
 
+import model.BookEdition;
 import model.Bookshelf;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ImportBookEdOperation extends ImportOperation {
+
     public ImportBookEdOperation(Bookshelf bookshelf) {
         createButton();
         initializeButtonAppearance();
@@ -32,9 +36,66 @@ public class ImportBookEdOperation extends ImportOperation {
             super(bookshelf);
         }
 
-        // TODO: implementation
+        // TODO: may need exception handling
         public void actionPerformed(ActionEvent e) {
-            System.out.println("Holy cow!");
+            final int ADDEDITIONDIALOGUEWIDTH = 400;
+            final int ADDEDITIONDIALOGUEHEIGHT = 200;
+            final int ROWNUM = 6;
+            final int COLNUM = 4;
+            final int HORIZSPACING = 2;
+            final int VERTISPACING = 2;
+            JTextField bookName = new JTextField();
+            JTextField authorName = new JTextField();
+            JTextField publisher = new JTextField();
+            JTextField yearOfPublish = new JTextField();
+            JTextField isbn = new JTextField();
+            JButton cancel = new JButton("Cancel");
+            JButton confirm = new JButton("Confirm");
+
+            JFrame addEditionDialogue = new JFrame("Add edition");
+            addEditionDialogue.setLayout(new BorderLayout());
+            addEditionDialogue.setMinimumSize(new Dimension(ADDEDITIONDIALOGUEWIDTH, ADDEDITIONDIALOGUEHEIGHT));
+            addEditionDialogue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            addEditionDialogue.setLocationRelativeTo(null);
+            addEditionDialogue.setVisible(true);
+            addEditionDialogue.setLayout(new GridLayout(ROWNUM,COLNUM,HORIZSPACING,VERTISPACING));
+            addEditionDialogue.add(new JLabel("Book Name"));
+            addEditionDialogue.add(bookName);
+            addEditionDialogue.add(new JLabel("Author Name"));
+            addEditionDialogue.add(authorName);
+            addEditionDialogue.add(new JLabel("Publisher"));
+            addEditionDialogue.add(publisher);
+            addEditionDialogue.add(new JLabel("Year Published"));
+            addEditionDialogue.add(yearOfPublish);
+            addEditionDialogue.add(new JLabel("ISBN"));
+            addEditionDialogue.add(isbn);
+            addEditionDialogue.add(cancel);
+            addEditionDialogue.add(confirm);
+
+            class ConfirmActionListener implements ActionListener{
+                private Bookshelf bookshelf;
+
+                public ConfirmActionListener(Bookshelf bookshelf){
+                    this.bookshelf = bookshelf;
+                }
+
+                public void actionPerformed(ActionEvent e) {
+                    BookEdition bookEd = new BookEdition(publisher.getText(),
+                            Integer.parseInt(yearOfPublish.getText()),
+                            Integer.parseInt(isbn.getText()));
+
+                    if (bookshelf.addEditionToBookshelf(bookName.getText(),
+                                                    authorName.getText(),
+                                                    bookEd)){
+                        System.out.println("Edition added!");
+                    }
+                    else{
+                        System.out.println("Oops, something's wrong, edition not added.");
+                    }
+                }
+            }
+
+            confirm.addActionListener(new ConfirmActionListener(bookshelf));
         }
     }
 }
